5e1bc778c890b1402ccb9f7a3d26afde7d96336e
seunghabaek <slug0326@naver.com>
2023-03-06 23:36:14 +0900

    add variables

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,23 +5,32 @@ import NaverProvider from "next-auth/providers/naver";
 import NextAuth from "next-auth/next";
 import { NextAuthOptions } from "next-auth";
 
+const facebookId: string = process.env.FACEBOOK_CLIENT_ID!;
+const facebookKey: string = process.env.FACEBOOK_CLIENT_SECRET!;
+const googleId: string = process.env.GOOGLE_CLIENT_ID!;
+const googleKey: string = process.env.GOOGLE_CLIENT_SECRET!;
+const kakaoId: string = process.env.KAKAO_CLIENT_ID!;
+const kakaoKey: string = process.env.KAKAO_CLIENT_SECRET!;
+const naverId: string = process.env.NAVER_CLIENT_ID!;
+const naverKey: string = process.env.NAVER_CLIENT_SECRET!;
+
 export const authOptions: NextAuthOptions = {
   providers: [
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      clientId: facebookId,
+      clientSecret: facebookKey,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: googleId,
+      clientSecret: googleKey,
     }),
     KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      clientId: kakaoId,
+      clientSecret: kakaoKey,
     }),
     NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID,
-      clientSecret: process.env.NAVER_CLIENT_SECRET,
+      clientId: naverId,
+      clientSecret: naverKey,
     }),
   ],
   secret: process.env.JWT_SECRET,
